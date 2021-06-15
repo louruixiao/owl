77ae4a5f2c4770390a502f8ee211f6b2a89b11ec
louruixiao <775674502@qq.com>
2021-06-15 23:17:11 +0800

    docs(docs): 测试algolia搜索

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -6,7 +6,6 @@ module.exports = {
 	themeConfig: {
 		repo: 'https://github.com/louruixiao/owl.git',
 		docsDir: 'docs',
-		locals: [],
 		editLinks: true,
 		editLinkText: 'Edit this page on GitHub',
 		lastUpdated: 'Last Updated',
@@ -15,18 +14,46 @@ module.exports = {
 			apiKey: '20da7e9a322b8eb2b5f42e378bfbdebc',
 			indexName: 'yiird_owl'
 		},
-		nav: [
-			{ text: 'Guide', link: '/guide/' },
-			{
-				text: 'Examples',
-				link: '/examples',
-				activeMatch: '^/$|^/examples/'
+		locales: {
+			'/': {
+				selectText: '选择语言',
+				label: '简体中文',
+				ariaLabel: '选择语言',
+				nav: [
+					{ text: '指南', link: '/guide/', activeMatch: '^/$|^/guide/' },
+					{
+						text: '样例',
+						link: '/examples/',
+						activeMatch: '^/$|^/examples/'
+					}
+				],
+				sidebar: {
+					'/guide/': getGuideSidebar(),
+					'/examples/': getExampleSidebar()
+				}
 			}
-		],
-
-		sidebar: {
-			'/guide/': getGuideSidebar(),
-			'/examples/': getExampleSidebar()
+			/* '/en/': {
+				// text for the language dropdown
+				selectText: 'Languages',
+				// label for this locale in the language dropdown
+				label: 'English',
+				// Aria Label for locale in the dropdown
+				ariaLabel: 'Languages',
+				nav: [
+					{ text: 'Guide', link: '/guide/' },
+					{
+						text: 'Examples',
+						link: '/examples',
+						activeMatch: '^/$|^/examples/'
+					}
+				],
+				sidebar: {
+					'/zh/': [
+					],
+					'/zh/nested/': [
+					]
+				}
+			} */
 		}
 	}
 };
@@ -40,6 +67,10 @@ function getGuideSidebar() {
 }
 function getExampleSidebar() {
 	return [
+		{
+			text: 'xx',
+			children: [{ text: 'xx', link: '/' }]
+		},
 		{
 			text: '基础',
 			children: [{ text: '按钮', link: '/examples/button' }]
